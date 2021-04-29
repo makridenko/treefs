507229451d0a3fc19c693ba3414f39aec20c6f73
Alex M <makridenko.a@yandex.ru>
2021-04-29 14:09:42 +0300

    add init and exit methods for tree filesystem

--- treefs.c
+++ treefs.c
@@ -6,14 +6,23 @@ MODULE_DESCRIPTION("My kernel module");
 MODULE_AUTHOR("Alexey Makridenko");
 MODULE_LICENSE("GPL");
 
-static int __init exmpl_init(void) {
-    printk(KERN_INFO "Hi kernel\n");
+
+static int __init treefs_init(void) {
+    int err;
+
+    err = register_filesyatem(&treefs_type);
+    if (err) {
+        printk(LOG_LEVEL "register_filesystem failed\n");
+        return err;
+    }
+
     return 0;
 }
 
-static void __exit exmpl_exit(void) {
-    printk(KERN_INFO "Bye\n");
+static void __exit treefs_exit(void) {
+    unregister_filesystem(&treefs_type);
 }
 
-module_init(exmpl_init);
-module_exit(exmpl_exit);
+
+module_init(treefs_init);
+module_exit(treefs_exit);
